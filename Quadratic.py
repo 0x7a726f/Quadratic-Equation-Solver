3d33be33a7be8a67850e2c2d016e536be6c6d66a
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2023-08-09 19:13:30 +0300

    Update Quadratic.py

--- Quadratic.py
+++ Quadratic.py
@@ -1,7 +1,7 @@
 def main(e):
     try:
         arr = [float(input("----------------------------\n\nax^2 + bx + c = 0\nInput a:\n>>")),float(input("Input b:\n>>")),float(input("Input c:\n>>"))] if e == 0 else [float(input("\nError...\n----------------------------\n\nax^2 + bx + c = 0\nInput a:\n>>")),float(input("Input b:\n>>")),float(input("Input c:\n>>"))]
-        print((lambda result, a=arr[0] , b=arr[1] , c=arr[2] : str("X= " + str((((-b)+(result)**(1/2))/(2*a)))  + " , " + str((((-b)-(result)**(1/2))/(2*a)))) if result > 0 else "X= " + str((((-b))/(2*a))))(arr[1]**2 -4*arr[0]*arr[2]),"\n") if (arr[1]**2 -4*arr[0]*arr[2]) >= 0 else print("Equasion is impossible\n")
+        print((lambda result, a=arr[0] , b=arr[1] , c=arr[2] : str("X= " + str((((-b)+(result)**(1/2))/(2*a)))  + " , " + str((((-b)-(result)**(1/2))/(2*a)))) if result > 0 else "X= " + str((((-b))/(2*a))))(b**2 -4*a*c),"\n") if (b**2 -4*a*c) >= 0 else print("Equasion is impossible\n")
         main(0)
     except:
         main(1)
